e52bacca551d46c18ec20e9a78b7c26b66d6a057
Alex Yuly <alexyuly2@gmail.com>
2019-09-30 12:04:24 -0700

    Add Checkers exercise

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # front-end-interview
+
 Front-End Interview Exercises
+
+## Instructions
+
+Run `yarn start` to open the interview exercises in a new browser window.
+
+### Checkers
+
+You are given a React app that renders a Checkers board.
+
+Implement these user requirements, to create a functional Checkers game.
+
+1. Add the ability for a user to drag and drop a piece to any space on the board.
+
+2. Add the ability for a user to drag and drop a piece into _a valid space on the board_.
+
+3. Add the ability to end one player's turn and begin the other's or end the game.
---
+++ stories/Checkers.stories.js
@@ -0,0 +1,99 @@
+import React from "react";
+import "./style.css";
+
+// This is used by Storybook:
+export default {
+  title: "Checkers"
+};
+
+// This is the main app:
+export const Checkers = () => {
+  return <Board size={400} />;
+};
+
+class Board extends React.Component {
+  state = {
+    board: [
+      [0, 1, 0, 1, 0, 1, 0, 1],
+      [1, 0, 1, 0, 1, 0, 1, 0],
+      [0, 1, 0, 1, 0, 1, 0, 1],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [2, 0, 2, 0, 2, 0, 2, 0],
+      [0, 2, 0, 2, 0, 2, 0, 2],
+      [2, 0, 2, 0, 2, 0, 2, 0]
+    ]
+  };
+
+  render() {
+    const spaceSize = this.props.size / 8;
+
+    return (
+      <svg
+        height={this.props.size}
+        width={this.props.size}
+        viewBox={`0 0 ${this.props.size} ${this.props.size}`}
+      >
+        {this.state.board.map((row, y) => {
+          const isEvenRow = y % 2;
+          return row.map((space, x) => {
+            const isEvenSpace = x % 2;
+            const spaceX = spaceSize * x;
+            const spaceY = spaceSize * y;
+            const pieceRadius = spaceSize / 2;
+            return (
+              <Space
+                key={x}
+                shade={
+                  (isEvenSpace && !isEvenRow) || (!isEvenSpace && isEvenRow)
+                }
+                size={spaceSize}
+                x={spaceX}
+                y={spaceY}
+              >
+                {space === 0 ? null : ( // The space is empty.
+                  <Piece
+                    player={space}
+                    r={pieceRadius * 0.75}
+                    cx={spaceX + pieceRadius}
+                    cy={spaceY + pieceRadius}
+                  />
+                )}
+              </Space>
+            );
+          });
+        })}
+      </svg>
+    );
+  }
+}
+
+class Space extends React.Component {
+  render() {
+    return (
+      <g>
+        <rect
+          fill={this.props.shade ? "green" : "lightgray"}
+          height={50}
+          width={50}
+          x={this.props.x}
+          y={this.props.y}
+        />
+        {this.props.children}
+      </g>
+    );
+  }
+}
+
+class Piece extends React.Component {
+  render() {
+    return (
+      <circle
+        fill={this.props.player === 1 ? "white" : "red"}
+        r={this.props.r}
+        cx={this.props.cx}
+        cy={this.props.cy}
+      />
+    );
+  }
+}
